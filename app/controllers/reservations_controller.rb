88f124abe1e401277901a899e780993db2fe8556
Baudin-Laurencin <abaudin@gmail.com>
2017-11-02 14:05:16 +0000

    Modification de reservations_controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,17 +1,17 @@
 class ReservationsController < Application Controller
 
+
+  def create
   @reservation = current_user.reservations.create(reservation_params)       
   redirect_to @reservation.room, notice: " Votre réservation a été acceptée ! " 
+ 
   end
-
 
 
 private
 
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id, :user_id)
-    
-
   end
 
 
